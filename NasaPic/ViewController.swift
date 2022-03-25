bff8fdf48e988fd9a2d11e93491036cb81b9c4e4
Apoorv Trikha <apoorv.trikha@sap.com>
2022-03-25 07:20:39 +0530

    Adding Basic UI

--- NasaPic/ViewController.swift
+++ NasaPic/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleView: UILabel!
+    @IBOutlet weak var descriptionView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
